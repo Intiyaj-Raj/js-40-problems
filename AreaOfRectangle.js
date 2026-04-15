e83533f1dec40181efc1586cb202f7567bb16c5f
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-15 10:53:57 +0530

    find area of rectangle

---
+++ AreaOfRectangle.js
@@ -0,0 +1,17 @@
+function calcArea(length, breadth) {
+    let area = length * breadth;
+    console.log("Area of Rectangle: " + area)
+}
+
+calcArea(10, "20")
+calcArea("10", "20")
+calcArea(10, 20)
+calcArea(10, true)
+calcArea(10, false)
+
+calcArea(5, 2.1)
+calcArea(5)
+calcArea(5, NaN)
+
+
+
