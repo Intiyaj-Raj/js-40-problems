9084a8badc7a876a3787b64b8c52f0efe4562d68
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-04-17 01:38:19 +0530

    calculate factorial using recursion

--- factorial.js
+++ factorial.js
@@ -16,6 +16,20 @@ function clacFact(num) {
 
 }
 
+
+// find factorial using recursion 
+
+
+function usignRecursion(n) {
+
+    if (n === 0 || n === 1) {
+        return 1
+    }
+
+    return n * usignRecursion(n - 1);
+}
+
+
 clacFact(5)
 clacFact(1)
 clacFact(0)
@@ -27,3 +41,8 @@ clacFact(5.3)
 clacFact(5.7)
 clacFact("a")
 clacFact(true)
+
+
+console.log("5! = " + usignRecursion(5))
+console.log("3! = " + usignRecursion(3))
+console.log("4! = " + usignRecursion(4))
