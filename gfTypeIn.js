eb4ac2f3f60bcf4959dcf8f1e7e778e13f142fd0
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-05-08 20:15:45 +0530

    add gf type

---
+++ gfTypeIn.js
@@ -0,0 +1,50 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// You in blue
+rl.question("\x1b[34mYou:\x1b[0m ", (input) => {
+
+    setTimeout(() => {
+        console.log("\x1b[32mSeen\x1b[0m");
+
+        setTimeout(() => {
+            typeText("GF is typing...", () => {
+
+                setTimeout(() => {
+                    typeText("I don’t want to talk anymore...", () => {
+
+                        setTimeout(() => {
+                            typeText("You are blocked 🚫", () => {
+                                rl.close();
+                            }, "\x1b[31m");
+                        }, 2000);
+
+                    });
+                }, 1000);
+
+            }, "\x1b[34m");
+
+        }, 1000);
+
+    }, 1500);
+
+});
+
+function typeText(text, callback, color = "\x1b[0m") {
+    let i = 0;
+
+    let interval = setInterval(() => {
+        process.stdout.write(color + text.charAt(i) + "\x1b[0m");
+        i++;
+
+        if (i >= text.length) {
+            clearInterval(interval);
+            process.stdout.write("\n");
+            if (callback) callback();
+        }
+    }, 50);
+}
