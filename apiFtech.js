38a2d9484be6d5ef62772b5132d220a0e06293c5
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-06-05 23:27:14 +0530

    fetch api logic

--- apiFtech.js
+++ apiFtech.js
@@ -1,3 +1,15 @@
+// simple example
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
+
+// using async await
 async function getUsers() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
