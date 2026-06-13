d148508271e7202dafd9f263683de3dbb10a9c02
Intiyaj-Raj <intiyajansari8757@gmail.com>
2026-06-13 16:05:03 +0530

    fetch api

---
+++ catFactApi.js
@@ -0,0 +1,30 @@
+// let url = "https://catfact.ninja/fact";
+// async function getFacts() {
+//   try {
+//     let res = await fetch(url);
+//     let data = await res.json();
+//     console.log(data.fact);
+//   } catch (e) {
+//     console.log("error ", e);
+//   }
+
+//   console.log("bye");
+// }
+
+// getFacts();
+
+// dog api
+
+// let dogUrl = "https://dog.ceo/api/breeds/image/random";
+
+// async function getDogImg() {
+//   try {
+//     let res = await fetch(dogUrl);
+//     let data = await res.json();
+//     console.log(data.message);
+//   } catch {
+//     console.log("error");
+//   }
+// }
+
+// getDogImg();
